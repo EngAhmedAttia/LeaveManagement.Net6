31c1209db9a6747e6a1722a12be6d84e2d83af79
Ahmed <eng.ahmed.attia@outlook.com>
2022-12-10 10:33:50 +0200

    Added Database Tables And Migrations, End Implement Database Section

---
+++ LeaveManagement.web/Data/LeaveAllocation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.web.Data
+{
+    public class LeaveAllocation:BaseEntity
+    {
+       
+
+        public int NumberOfDayes { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+
+       
+    }
+}
