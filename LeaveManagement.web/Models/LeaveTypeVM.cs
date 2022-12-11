5616be7812bfe36c8876223343c76f343d7d431e
Ahmed <eng.ahmed.attia@outlook.com>
2022-12-11 09:15:54 +0200

    Modified the Crud Opererations and added datatables and swal , End of Leave Type Management Module

---
+++ LeaveManagement.web/Models/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Leave Type Name")]
+        [Required]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1,10 ,ErrorMessage ="Please Enter Number Between 1-10 ")]
+
+        public int DefaultDays { get; set; }
+    }
+}
