472ecb8114504b601dc197906f4587cf6b09dd10
Ahmed <eng.ahmed.attia@outlook.com>
2022-12-11 12:39:27 +0200

    Added leave Type Repository , End of Repository Pattern Module

--- LeaveManagement.web/Program.cs
+++ LeaveManagement.web/Program.cs
@@ -1,5 +1,7 @@
 using LeaveManagement.web.Configurations;
+using LeaveManagement.web.Contracts;
 using LeaveManagement.web.Data;
+using LeaveManagement.web.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +17,9 @@ builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireC
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddScoped<ILeaveTypeRepository,LeaveTypeRepository>();
 
 builder.Services.AddControllersWithViews();
 
